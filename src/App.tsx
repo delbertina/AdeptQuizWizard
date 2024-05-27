4321de9c69ff1e56c733f4fd2b8cf4957fb89297
delbertina <6349928+delbertina@users.noreply.github.com>
2024-05-27 17:01:29 -0500

    Update styles in prep for real data

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import "./App.scss";
 import { Add } from "@mui/icons-material";
 import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
 import QuizCard from "./components/QuizCard/QuizCard";
+import QuizEditDialog from "./components/QuizEditDialog/QuizEditDialog";
+import QuizViewDialog from "./components/QuizViewDialog/QuizViewDialog";
 
 function App() {
+  const [isEditDialogOpen, setIsEditDialogOpen] = useState<boolean>(false);
+  const [isViewDialogOpen, setIsViewDialogOpen] = useState<boolean>(false);
+  const [currentQuiz, setCurrentQuiz] = useState<number>(0)
   const [quizzes, setQuizzes] = useState<Array<any>>([
     "test",
     "test2",
@@ -16,6 +21,29 @@ function App() {
     "test2",
     "test3",
   ]);
+
+  const handleEditDialogOpen = (id: number): void => {
+    setIsEditDialogOpen(true);
+    setCurrentQuiz(id);
+  }
+
+  const handleEditDialogClose = (id?: number): void => {
+    console.log("edit dialog close: ", id);
+    setIsEditDialogOpen(false);
+    setCurrentQuiz(0);
+  }
+
+  const handleViewDialogOpen = (id: number): void => {
+    setIsViewDialogOpen(true);
+    setCurrentQuiz(id);
+  }
+
+  const handleViewDialogClose = (id?: number): void => {
+    console.log("edit dialog close: ", id);
+    setIsViewDialogOpen(false);
+    setCurrentQuiz(0);
+  }
+
   return (
     <div className="App">
       <div className="App-content">
@@ -38,12 +66,23 @@ function App() {
           {quizzes.map((quiz, index) => (
             <QuizCard
               key={index}
-              title={quiz}
-              onClick={() => console.log("clicked quiz card: " + index)}
+              title={"super duper long quiz name"}
+              description="something a long description of the quiz something a long description of the quiz something a long description of the quiz something a long description of the quiz"
+              score="94%"
+              onClick={() => handleViewDialogOpen(index)}
+              onEditClick={() => handleEditDialogOpen(index)}
             ></QuizCard>
           ))}
         </div>
       </div>
+      <QuizEditDialog
+        isDialogOpen={isEditDialogOpen}
+        handleDialogClose={(index?: number) => handleEditDialogClose(index)}
+      />
+      <QuizViewDialog
+        isDialogOpen={isViewDialogOpen}
+        handleDialogClose={(index?: number) => handleViewDialogClose(index)}
+      />
     </div>
   );
 }
--- src/components/QuizCard/QuizCard.tsx
+++ src/components/QuizCard/QuizCard.tsx
@@ -1,13 +1,27 @@
 import { Edit } from "@mui/icons-material";
 import "./QuizCard.scss";
-import { Card, CardContent, CardHeader, IconButton } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  IconButton,
+  Typography,
+} from "@mui/material";
 
 export interface QuizCardProps {
   title: string;
+  description: string;
+  score: string;
   onClick: () => void;
+  onEditClick: () => void;
 }
 
 function QuizCard(props: QuizCardProps) {
+  const handleEditClick = (e: React.MouseEvent): void => {
+    e.stopPropagation();
+    props.onEditClick();
+  };
+
   return (
     <>
       <Card onClick={() => props.onClick()} className="quiz-card">
@@ -15,12 +29,39 @@ function QuizCard(props: QuizCardProps) {
           className="quiz-card-header"
           title={props.title}
           action={
-            <IconButton aria-label="edit">
+            <IconButton
+              aria-label="edit"
+              onClick={(e: React.MouseEvent) => handleEditClick(e)}
+            >
               <Edit />
             </IconButton>
           }
         ></CardHeader>
-        <CardContent className="quiz-card-content"></CardContent>
+        <CardContent className="quiz-card-content">
+          <Typography
+            sx={{
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              display: "-webkit-box",
+              WebkitLineClamp: "3",
+              WebkitBoxOrient: "vertical",
+            }}
+            gutterBottom
+            id="quiz-card-description"
+            variant="body1"
+            component="div"
+          >
+            {props.description}
+          </Typography>
+          <Typography
+            sx={{ fontWeight: "bold" }}
+            id="quiz-card-score"
+            variant="body1"
+            component="div"
+          >
+            {"Last Score: " + props.score}
+          </Typography>
+        </CardContent>
       </Card>
     </>
   );
