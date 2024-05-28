638c73cb38f663301627f22067eadb6c345aaab7
delbertina <6349928+delbertina@users.noreply.github.com>
2024-05-27 21:18:58 -0500

    Add delete questions and answers in edit menu

--- src/components/QuizEditDialog/QuizEditDialog.tsx
+++ src/components/QuizEditDialog/QuizEditDialog.tsx
@@ -5,6 +5,7 @@ import {
   ArrowUpward,
   Circle,
   CircleOutlined,
+  Delete,
   Edit,
 } from "@mui/icons-material";
 import {
@@ -28,22 +29,27 @@ export interface QuizEditDialogProps {
 }
 
 function QuizEditDialog(props: QuizEditDialogProps) {
-  const [isEditQuizTitleDialogOpen, setIsEditQuizTitleDialogOpen] = useState<boolean>(false);
-  const [isEditQuizDescDialogOpen, setIsEditQuizDescDialogOpen] = useState<boolean>(false);
-  const [isEditQuesTextDialogOpen, setIsEditQuesTextDialogOpen] = useState<boolean>(false);
-  const [isEditAnsTextDialogOpen, setIsEditAnsTextDialogOpen] = useState<boolean>(false);
+  const [isEditQuizTitleDialogOpen, setIsEditQuizTitleDialogOpen] =
+    useState<boolean>(false);
+  const [isEditQuizDescDialogOpen, setIsEditQuizDescDialogOpen] =
+    useState<boolean>(false);
+  const [isEditQuesTextDialogOpen, setIsEditQuesTextDialogOpen] =
+    useState<boolean>(false);
+  const [isEditAnsTextDialogOpen, setIsEditAnsTextDialogOpen] =
+    useState<boolean>(false);
   const [quiz, setQuiz] = useState<Quiz | undefined>();
   const [editQIndex, setEditQIndex] = useState<number>(-1);
   const [editAIndex, setEditAIndex] = useState<number>(-1);
+  const [isSubmitDisabled, setIsSubmitDisabled] = useState<boolean>(true);
 
   const handleEditQuizTitleOpen = (): void => {
     setIsEditQuizTitleDialogOpen(true);
-  }
+  };
 
   const handleEditQuizTitle = (edited?: string): void => {
     setIsEditQuizTitleDialogOpen(false);
     const tempQuiz = quiz;
-    if(!!tempQuiz && !!edited) {
+    if (!!tempQuiz && !!edited) {
       tempQuiz.title = edited;
       setQuiz(tempQuiz);
     }
@@ -56,7 +62,7 @@ function QuizEditDialog(props: QuizEditDialogProps) {
   const handleEditQuizDescription = (edited?: string): void => {
     setIsEditQuizDescDialogOpen(false);
     const tempQuiz = quiz;
-    if(!!tempQuiz && !!edited) {
+    if (!!tempQuiz && !!edited) {
       tempQuiz.description = edited;
       setQuiz(tempQuiz);
     }
@@ -65,12 +71,12 @@ function QuizEditDialog(props: QuizEditDialogProps) {
   const handleEditQuestionTextOpen = (qIndex: number): void => {
     setIsEditQuesTextDialogOpen(true);
     setEditQIndex(qIndex);
-  }
+  };
 
   const handleEditQuestionText = (edited?: string): void => {
     setIsEditQuesTextDialogOpen(false);
     const tempQuiz = quiz;
-    if(!!tempQuiz && !!edited) {
+    if (!!tempQuiz && !!edited) {
       tempQuiz.questions[editQIndex].text = edited;
       setQuiz(tempQuiz);
     }
@@ -81,12 +87,12 @@ function QuizEditDialog(props: QuizEditDialogProps) {
     setIsEditAnsTextDialogOpen(true);
     setEditQIndex(qIndex);
     setEditAIndex(aIndex);
-  }
+  };
 
   const handleEditAnswerText = (edited?: string): void => {
     setIsEditAnsTextDialogOpen(false);
     const tempQuiz = quiz;
-    if(!!tempQuiz && !!edited) {
+    if (!!tempQuiz && !!edited) {
       tempQuiz.questions[editQIndex].answers[editAIndex].text = edited;
       setQuiz(tempQuiz);
     }
@@ -95,36 +101,85 @@ function QuizEditDialog(props: QuizEditDialogProps) {
   };
 
   const handleAddQuestion = (): void => {
-    if (!quiz){
+    if (!quiz) {
       return;
     }
     const tempQuiz = quiz;
-    const maxIndex = Math.max(...tempQuiz.questions.map(question => question.id));
-    const newQuestion = {id: maxIndex + 1, text: "New Question", answers: [], feedbackTrue: "Correct!", feedbackFalse: "Incorrect!", correctAnswerId: -1};
+    const maxIndex = Math.max(
+      ...tempQuiz.questions.map((question) => question.id)
+    );
+    const newQuestion = {
+      id: maxIndex + 1,
+      text: "New Question",
+      answers: [],
+      feedbackTrue: "Correct!",
+      feedbackFalse: "Incorrect!",
+      correctAnswerId: -1,
+    };
     tempQuiz.questions = [newQuestion, ...tempQuiz?.questions];
-    setQuiz({...tempQuiz});
-  }
+    setQuiz({ ...tempQuiz });
+  };
+
+  const handleDeleteQuestion = (qIndex: number): void => {
+    if (!quiz) {
+      return;
+    }
+    const tempQuiz = quiz;
+    tempQuiz.questions = tempQuiz.questions.filter(
+      (question) => question.id !== qIndex
+    );
+    setQuiz({ ...tempQuiz });
+  };
 
   const handleAddAnswer = (qIndex: number): void => {
-    if (!quiz){
+    if (!quiz) {
       return;
     }
     const tempQuiz = quiz;
-    const maxIndex = Math.max(...tempQuiz.questions[qIndex].answers.map(answer => answer.id), 0);
+    const maxIndex = Math.max(
+      ...tempQuiz.questions[qIndex].answers.map((answer) => answer.id),
+      0
+    );
     console.log("handle add answer", maxIndex);
-    tempQuiz?.questions[qIndex].answers.push({id: maxIndex + 1, text: "New Answer"});
-    setQuiz({...tempQuiz});
-  }
+    tempQuiz?.questions[qIndex].answers.push({
+      id: maxIndex + 1,
+      text: "New Answer",
+    });
+    setQuiz({ ...tempQuiz });
+  };
+
+  const handleDeleteAnswer = (qIndex: number, aIndex: number): void => {
+    if (!quiz) {
+      return;
+    }
+    const tempQuiz = quiz;
+    tempQuiz.questions[qIndex].answers = tempQuiz.questions[
+      qIndex
+    ].answers.filter((answer) => answer.id !== aIndex);
+    setQuiz({ ...tempQuiz });
+  };
 
   const handleSelectCorrectAnswer = (qindex: number, aindex: number): void => {
     console.log("handle select correct ", qindex, aindex);
-    if(!quiz) {
+    if (!quiz) {
       return;
     }
     const tempQuiz = quiz;
     tempQuiz.questions[qindex].correctAnswerId = aindex;
-    setQuiz({...tempQuiz});
-  }
+    setQuiz({ ...tempQuiz });
+  };
+
+  useEffect(() => {
+    setIsSubmitDisabled(
+      !quiz ||
+        quiz.questions.length === 0 ||
+        quiz.questions
+          .map((question) => question.answers.length)
+          .indexOf(0) !== -1
+        || quiz.questions.map((question) => question.correctAnswerId)
+        .indexOf(-1) !== -1
+    );
+  }, [quiz]);
 
   useEffect(() => {
     setQuiz(props.quiz);
@@ -160,15 +215,6 @@ function QuizEditDialog(props: QuizEditDialogProps) {
             >
               Add, edit, and remove quiz questions.
             </Typography>
-          </div>
-          <div>
-            <IconButton
-              aria-label="add question"
-              color="success"
-              onClick={() => handleAddQuestion()}
-            >
-              <Add />
-            </IconButton>
           </div>
         </DialogTitle>
         <DialogContent dividers={true} id="quiz-edit-dialog-content">
@@ -208,88 +254,125 @@ function QuizEditDialog(props: QuizEditDialogProps) {
             </IconButton>
           </div>
           <Divider />
+          <div className="quiz-edit-dialog-content-list-header row">
+            <Typography variant="h5" component="div">
+              Questions
+            </Typography>
+            <IconButton
+              aria-label="add question"
+              color="success"
+              onClick={() => handleAddQuestion()}
+            >
+              <Add />
+            </IconButton>
+          </div>
+          <Divider />
           {/* list of existing questions */}
           <div className="quiz-edit-dialog-content-list">
             {quiz?.questions.map((question, qindex) => (
-            <div className="quiz-edit-dialog-content-question" key={qindex}>
-              <div className="quiz-edit-dialog-content-question-text row">
-                <div className="quiz-edit-dialog-content-question-text-start-actions">
-                  <IconButton disabled={true}>
-                    <ArrowUpward />
-                  </IconButton>
-                  <IconButton disabled={true}>
-                    <ArrowDownward />
-                  </IconButton>
-                </div>
-                <Typography
-                  gutterBottom
-                  id="quiz-edit-dialog-content-question-text"
-                  variant="h6"
-                  component="div"
-                >
-                  {question.text}
-                </Typography>
-                <div className="quiz-edit-dialog-content-question-text-end-actions">
-                  <IconButton color="success" onClick={() => handleAddAnswer(qindex)}>
-                    <Add />
-                  </IconButton>
-                  <IconButton aria-label="edit-text" color="warning" onClick={() => handleEditQuestionTextOpen(qindex)}>
-                    <Edit />
-                  </IconButton>
-                  {/* <IconButton
-                    aria-label="delete question"
-                    color="error"
-                    disabled={true}
-                    onClick={() => console.log("delete question clicked")}
-                  >
-                    <Delete />
-                  </IconButton> */}
-                </div>
-              </div>
-            
-              <div className="quiz-edit-dialog-content-question-answers">
-                {question.answers.map((answer, aindex) => (
-                <div className="quiz-edit-dialog-content-question-answer-row row" key={aindex}>
-                  <div className="quiz-edit-dialog-content-question-answer-row-start-actions">
-                    <IconButton onClick={() => handleSelectCorrectAnswer(qindex, answer.id)}>
-                      {answer.id === quiz.questions[qindex].correctAnswerId && (
-                        <Circle />
-                      )}
-                      {answer.id !== quiz.questions[qindex].correctAnswerId && (
-                      <CircleOutlined />
-                      )}
+              <div className="quiz-edit-dialog-content-question" key={qindex}>
+                <div className="quiz-edit-dialog-content-question-text row">
+                  <div className="quiz-edit-dialog-content-question-text-start-actions">
+                    <IconButton disabled={true}>
+                      <ArrowUpward />
+                    </IconButton>
+                    <IconButton disabled={true}>
+                      <ArrowDownward />
                     </IconButton>
                   </div>
                   <Typography
-                    className="quiz-edit-dialog-content-question-answer-row-text"
-                    variant="body1"
+                    gutterBottom
+                    id="quiz-edit-dialog-content-question-text"
+                    variant="h6"
                     component="div"
                   >
-                    {answer.text}
+                    {question.text}
                   </Typography>
-                  <div className="quiz-edit-dialog-content-question-answer-row-end-actions">
-                    <IconButton aria-label="edit-text" color="warning" onClick={() => handleEditAnswerTextOpen(qindex, aindex)}>
+                  <div className="quiz-edit-dialog-content-question-text-end-actions">
+                    <IconButton
+                      color="success"
+                      onClick={() => handleAddAnswer(qindex)}
+                    >
+                      <Add />
+                    </IconButton>
+                    <IconButton
+                      aria-label="edit-text"
+                      color="warning"
+                      onClick={() => handleEditQuestionTextOpen(qindex)}
+                    >
                       <Edit />
                     </IconButton>
-                    {/* <IconButton
+                    <IconButton
                       aria-label="delete question"
                       color="error"
-                      onClick={() => console.log("delete question clicked")}
+                      onClick={() => handleDeleteQuestion(question.id)}
                     >
                       <Delete />
-                    </IconButton> */}
+                    </IconButton>
                   </div>
                 </div>
-                ))}
+                <div className="quiz-edit-dialog-content-question-answers">
+                  {question.answers.map((answer, aindex) => (
+                    <div
+                      className="quiz-edit-dialog-content-question-answer-row row"
+                      key={aindex}
+                    >
+                      <div className="quiz-edit-dialog-content-question-answer-row-start-actions">
+                        <IconButton
+                          onClick={() =>
+                            handleSelectCorrectAnswer(qindex, answer.id)
+                          }
+                        >
+                          {answer.id ===
+                            quiz.questions[qindex].correctAnswerId && (
+                            <Circle />
+                          )}
+                          {answer.id !==
+                            quiz.questions[qindex].correctAnswerId && (
+                            <CircleOutlined />
+                          )}
+                        </IconButton>
+                      </div>
+                      <Typography
+                        className="quiz-edit-dialog-content-question-answer-row-text"
+                        variant="body1"
+                        component="div"
+                      >
+                        {answer.text}
+                      </Typography>
+                      <div className="quiz-edit-dialog-content-question-answer-row-end-actions">
+                        <IconButton
+                          aria-label="edit-text"
+                          color="warning"
+                          onClick={() =>
+                            handleEditAnswerTextOpen(qindex, aindex)
+                          }
+                        >
+                          <Edit />
+                        </IconButton>
+                        <IconButton
+                          aria-label="delete question"
+                          color="error"
+                          onClick={() => handleDeleteAnswer(qindex, answer.id)}
+                        >
+                          <Delete />
+                        </IconButton>
+                      </div>
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
             ))}
           </div>
           {/* footer close & submit */}
         </DialogContent>
         <DialogActions>
           <Button onClick={() => props.handleDialogClose()}>Cancel</Button>
-          <Button variant="contained" onClick={() => props.handleDialogClose(quiz)}>
+          <Button
+            variant="contained"
+            onClick={() => props.handleDialogClose(quiz)}
+            disabled={isSubmitDisabled}
+          >
             Submit
           </Button>
         </DialogActions>
