6f999d18d379c3c9fee5a0e41f11e82cefd7359d
delbertina <6349928+delbertina@users.noreply.github.com>
2024-08-09 19:52:29 -0500

    Update tests for edit text dialog

--- src/components/EditTextDialog/EditTextDialog.test.tsx
+++ src/components/EditTextDialog/EditTextDialog.test.tsx
@@ -1,52 +1,118 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import EditTextDialog from "./EditTextDialog";
 import { store } from "../../store/store";
 import { Provider } from "react-redux";
 
-test('renders text field', () => {
-    render(<Provider store={store}><EditTextDialog 
+test("renders text field", () => {
+  render(
+    <Provider store={store}>
+      <EditTextDialog
         isDialogOpen={true}
         dialogTitle=""
         dialogDescription=""
         dialogFieldLabel=""
         dialogFieldValue=""
-        handleDialogClose={() => {}}/></Provider>);
-    const textField = screen.getByTestId("editedText");
-    expect(textField).toBeInTheDocument();
-  });
+        handleDialogClose={() => {}}
+      />
+    </Provider>
+  );
+  const textField = screen.getByTestId("editedText");
+  expect(textField).toBeInTheDocument();
+});
 
-  test('renders title', () => {
-    render(<Provider store={store}><EditTextDialog 
+test("renders title", () => {
+  render(
+    <Provider store={store}>
+      <EditTextDialog
         isDialogOpen={true}
         dialogTitle=""
         dialogDescription=""
         dialogFieldLabel=""
         dialogFieldValue=""
-        handleDialogClose={() => {}}/></Provider>);
-    const titleDisplay = screen.getByTestId("edit-text-dialog-header");
-    expect(titleDisplay).toBeInTheDocument();
-  });
+        handleDialogClose={() => {}}
+      />
+    </Provider>
+  );
+  const titleDisplay = screen.getByTestId("edit-text-dialog-header");
+  expect(titleDisplay).toBeInTheDocument();
+});
 
-  test('renders no description when blank', () => {
-    render(<Provider store={store}><EditTextDialog 
+test("renders no description when blank", () => {
+  render(
+    <Provider store={store}>
+      <EditTextDialog
         isDialogOpen={true}
         dialogTitle=""
         dialogDescription=""
         dialogFieldLabel=""
         dialogFieldValue=""
-        handleDialogClose={() => {}}/></Provider>);
-    const descriptionDisplay = screen.queryByTestId("edit-text-dialog-description");
-    expect(descriptionDisplay).toBeNull();
-  });
+        handleDialogClose={() => {}}
+      />
+    </Provider>
+  );
+  const descriptionDisplay = screen.queryByTestId(
+    "edit-text-dialog-description"
+  );
+  expect(descriptionDisplay).toBeNull();
+});
 
-  test('renders description', () => {
-    render(<Provider store={store}><EditTextDialog 
+test("renders description", () => {
+  render(
+    <Provider store={store}>
+      <EditTextDialog
         isDialogOpen={true}
         dialogTitle=""
         dialogDescription="Test Description"
         dialogFieldLabel=""
         dialogFieldValue=""
-        handleDialogClose={() => {}}/></Provider>);
-    const descriptionDisplay = screen.getByTestId("edit-text-dialog-description");
-    expect(descriptionDisplay).toBeInTheDocument();
+        handleDialogClose={() => {}}
+      />
+    </Provider>
+  );
+  const descriptionDisplay = screen.getByTestId("edit-text-dialog-description");
+  expect(descriptionDisplay).toBeInTheDocument();
+});
+
+test("submit button calls close", async () => {
+  const onSubmit = jest.fn();
+  render(
+    <Provider store={store}>
+      <EditTextDialog
+        isDialogOpen={true}
+        dialogTitle=""
+        dialogDescription=""
+        dialogFieldLabel=""
+        dialogFieldValue=""
+        handleDialogClose={() => onSubmit()}
+      />
+    </Provider>
+  );
+  const submitButton = screen.getByTestId("submit-button");
+  expect(submitButton).toBeInTheDocument();
+  fireEvent.click(submitButton);
+  await waitFor(() => {
+    expect(onSubmit).toBeCalledTimes(1);
   });
+});
+
+test("cancel button calls close", async () => {
+  const onSubmit = jest.fn();
+  render(
+    <Provider store={store}>
+      <EditTextDialog
+        isDialogOpen={true}
+        dialogTitle=""
+        dialogDescription=""
+        dialogFieldLabel=""
+        dialogFieldValue=""
+        handleDialogClose={() => onSubmit()}
+      />
+    </Provider>
+  );
+  const cancelButton = screen.getByTestId("cancel-button");
+  expect(cancelButton).toBeInTheDocument();
+  fireEvent.click(cancelButton);
+  await waitFor(() => {
+    expect(onSubmit).toBeCalledTimes(1);
+  });
+});
